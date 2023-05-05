e3a514efb46c3aab1fbe5744210341f00aa5c75b
hufaga <93388988+hufaga@users.noreply.github.com>
2023-05-05 17:47:25 +0900

    Update 콜라츠 추측.py

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -3,13 +3,13 @@ def solution(num):
     while True:
         if num==1:
             break  
-        if num%2==0:
+        if num%2==0: ## 짝수 홀수 구별 
             num=num//2
-            cnt+=1
+            cnt+=1  ## 실행될때마다 카운트
         else:
             num=num*3+1
             cnt+=1
-        if cnt >= 500:
+        if cnt >= 500: ## 500회 이상 반복시 -1 리턴
             return -1
         
     return cnt
