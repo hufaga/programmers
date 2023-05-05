0729c02078b0d2ddb1ef89dd305e43358c03dc40
hufaga <93388988+hufaga@users.noreply.github.com>
2023-05-05 17:49:07 +0900

    Update 제일 작은 수 제거하기.py

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
@@ -2,9 +2,9 @@ def solution(arr):
     answer = []
     
     
-    if len(arr) == 1:
+    if len(arr) == 1: ## 숫자가 하나밖에 없다면 -1리턴
         answer.append(-1)
-    else:
+    else: ## arr에 두개 이상 담고 있다면 제일 작은 숫자 제거
         arr.remove(min(arr))
         answer = arr
     return answer
