842af81b5acd0e70e35a61e6bc48117520550dca
hufaga <93388988+hufaga@users.noreply.github.com>
2023-05-05 17:38:45 +0900

    Update 없는 숫자 더하기.py

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -1,6 +1,6 @@
 def solution(numbers):
     answer = 0
     for i in range(10):
-        if i not in numbers:
+        if i not in numbers: ## 0부터 9까지 돌면서 numbers에 없는 숫자 answer에 더해주기
             answer += i
     return answer
