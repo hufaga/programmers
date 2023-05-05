e5162ce048f34b213a47d4f846748510ab3b9bef
hufaga <93388988+hufaga@users.noreply.github.com>
2023-05-05 17:41:03 +0900

    Update 음양 더하기.py

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
@@ -2,6 +2,6 @@ def solution(absolutes, signs):
     answer = 0
     for i in range(len(absolutes)):
         if signs[i]==False:
-            absolutes[i] = absolutes[i]*-1
-    answer = sum(absolutes)
+            absolutes[i] = absolutes[i]*-1 ##signs의 i번째가 false일때 -로 만들어주고 absolutes i번째로 치환
+    answer = sum(absolutes) 
     return answer
