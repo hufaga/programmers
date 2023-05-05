9c342fe35d283cdb90cfb8649a8a5fdfb264837d
hufaga <93388988+hufaga@users.noreply.github.com>
2023-05-05 17:45:06 +0900

    Update 핸드폰 번호 가리기.py

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
@@ -2,5 +2,5 @@ def solution(phone_number):
     answer = ''
     star = len(phone_number)-4
     end = phone_number[-4:]
-    answer = '*'*star + end 
+    answer = '*'*star + end  ## star에는 뒤 4개를 제외한 길이만큼 *을 만들고 end에는 phone_number의 뒷자리 4개의 숫자를 더해 문자열 생성
     return answer
