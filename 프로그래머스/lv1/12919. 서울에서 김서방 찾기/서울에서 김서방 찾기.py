40603d2692052923dfa92e77fb4557b4b2577373
hufaga <93388988+hufaga@users.noreply.github.com>
2023-05-05 17:53:42 +0900

    Update 서울에서 김서방 찾기.py

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -4,6 +4,6 @@ def solution(seoul):
     for i in seoul:
         
         if i == 'Kim':
-            return ('김서방은 '+ str(cnt)+'에 있다')
+            return ('김서방은 '+ str(cnt)+'에 있다') ##0번째도 포함하기 위해 cnt 후열 배치
         cnt+=1
     return answer
