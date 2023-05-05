4ab475dd4d3c691da7bf72d15e2dabccc2debf2f
hufaga <93388988+hufaga@users.noreply.github.com>
2023-05-05 17:56:32 +0900

    Update 나누어 떨어지는 숫자 배열.py

--- 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.py
+++ 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.py
@@ -1,9 +1,9 @@
 def solution(arr, divisor):
     answer = []
     for i in arr:
-        if i%divisor==0:
+        if i%divisor==0: ##나머지가 0일때만 answer에 추가
             answer.append(i)
-    answer.sort()
-    if not answer:
+    answer.sort() ##작은 숫자 먼저
+    if not answer: ## answer에 없다면 not false
         answer.append(-1)
     return answer
