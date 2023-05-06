074fbbbb3bce9fcd087141bc7dbc1337c03abdfa
hufaga <93388988+hufaga@users.noreply.github.com>
2023-05-06 12:11:12 +0900

    Update 약수의 개수와 덧셈.py

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -3,7 +3,7 @@ def solution(left, right):
     
     for i in range(left,right+1):
         
-        if i**0.5==int(i**0.5):
+        if i**0.5==int(i**0.5): ##제곱근이 정수로 나누어 떨어지면 약수의 개수가 홀수
             answer-=i
         else:
             answer+=i
