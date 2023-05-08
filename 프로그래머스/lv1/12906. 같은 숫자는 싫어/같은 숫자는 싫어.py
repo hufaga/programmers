a668c23895c1743f2782fd2d939d95c6087a42fa
hufaga <93388988+hufaga@users.noreply.github.com>
2023-05-08 17:29:36 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 147.50 ms, Memory: 27.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,12 @@
+def solution(arr):
+    answer=[]
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
+#     for i in range(len(arr)-1):
+#         if arr[i] == arr[i+1]:
+#             answer.remove(arr[i])
+    a = []
+    for i in arr:
+        if a[-1:] == [i]: continue
+        a.append(i)
+    return a
