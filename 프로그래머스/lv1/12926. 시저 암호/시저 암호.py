65dc11b3a4cc70d85f82cc14fd955718ad6720a8
hufaga <93388988+hufaga@users.noreply.github.com>
2023-05-12 17:38:39 +0900

    Update 시저 암호.py

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.py
@@ -3,15 +3,15 @@ def solution(s, n):
     sut=0
     mun=''
     for i in s:
-        if i == " ":
+        if i == " ":                          ## 공백은 공백으로 출력 
             answer+=i
             continue
-        if 64 < ord(i) <91 and ord(i)+n > 90:
+        if 64 < ord(i) <91 and ord(i)+n > 90: ## 대문자일때 ../ isupper 쓸 걸 그랬다..
             sut = (ord(i)+n)-26
             mun = chr(sut)
             answer+=mun
-            continue
-        if 96 < ord(i) < 123 and ord(i)+n > 122:
+            continue                          ## ascii code가 대문자 범위를 over하지 않을 때 나머지 구문 무시 
+        if 96 < ord(i) < 123 and ord(i)+n > 122: ## 소문자일때
             sut = (ord(i)+n)-26
             mun = chr(sut)
             answer+=mun
