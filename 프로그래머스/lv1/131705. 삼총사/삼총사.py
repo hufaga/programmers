edd2049414fec5842988303021409fbc53af5f94
hufaga <93388988+hufaga@users.noreply.github.com>
2023-05-12 17:32:38 +0900

    Update 삼총사.py

--- 프로그래머스/lv1/131705. 삼총사/삼총사.py
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -1,7 +1,7 @@
 import itertools
 def solution(number):
     answer = 0
-    for i in itertools.combinations(number,3):
+    for i in itertools.combinations(number,3): ##itertools.combinations를 사용하면 입력받은 데이터의 모든 경우의 수를 반환 
         if sum(i)==0:
             answer+=1
         
