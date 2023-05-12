0590d3c07cba70fd0d6d0cad26604fc3ffc5c4b3
hufaga <93388988+hufaga@users.noreply.github.com>
2023-05-12 17:51:40 +0900

    Update 최소직사각형.py

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
@@ -7,5 +7,5 @@ def solution(sizes):
     # j=max(lis).index
     # answer = sizes[j]
     
-    answer = max(max(x) for x in sizes) * max(min(y) for y in sizes)
+    answer = max(max(x) for x in sizes) * max(min(y) for y in sizes)  ##sizes 중에서 큰것들 중 가장 큰것 과 작은것들 중 가장 큰 것을 곱한 값을 return
     return answer
