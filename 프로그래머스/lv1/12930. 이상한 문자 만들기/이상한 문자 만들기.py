5ba0eddc0136065e4104100ecb2eaab3c4603aca
hufaga <93388988+hufaga@users.noreply.github.com>
2023-05-12 17:49:04 +0900

    Update 이상한 문자 만들기.py

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,36 +1,11 @@
 def solution(s):
     answer = ''
-    temp=0
-    cnt=0
-    # for i in s:
-    #     if cnt != 0:
-    #         answer+=' '*temp
-    #     cnt=0
-    #     temp=0
-    #     print(i)
-#     for j in s:
-#         if j==' ':
-#             cnt=0
-#             temp+=1
-#             print(temp)
-#         if cnt%2==0:
-#             j=j.upper()
-#             answer+=j
-#         else:
-#             j=j.lower()
-#             answer+=j
-            
-#         cnt+=1
-#         print(cnt)
-#     return answer
-def solution(s):
-    answer = ''
     new_list = s.split(' ')
-    for i in new_list:
-        for j in range(len(i)):
-            if j % 2 == 0:
-                answer += i[j].upper()
+    for i in new_list:            
+        for j in range(len(i)):         
+            if j % 2 == 0:               
+                answer += i[j].upper()      ## 짝수번째는 대문자로  
             else:
-                answer += i[j].lower()
-        answer+= ' '
+                answer += i[j].lower()      ## 홀수번째는 소문자로 
+        answer+= ' '                        
     return answer[0:-1]
