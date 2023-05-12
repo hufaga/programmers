f5aad4dafdb48c267577897e7bdae45134069195
hufaga <93388988+hufaga@users.noreply.github.com>
2023-05-12 17:41:59 +0900

    Update 같은 숫자는 싫어.py

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,12 +1,10 @@
 def solution(arr):
-    answer=[]
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
+# [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
 #     for i in range(len(arr)-1):
-#         if arr[i] == arr[i+1]:
+#         if arr[i] == arr[i+1]:     ## 삭제하는 것은 리스트의 길이가 줄어들어 out of range
 #             answer.remove(arr[i])
-    a = []
+    answer = []
     for i in arr:
-        if a[-1:] == [i]: continue
-        a.append(i)
-    return a
+        if answer[-1:] == [i]: continue ##같은 숫자가 있다면 나머지 구문 무시 (중복되는 숫자 제거)
+        answer.append(i)
+    return answer
