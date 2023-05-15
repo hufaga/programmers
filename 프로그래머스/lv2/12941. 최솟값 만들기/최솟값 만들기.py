4bef27cf85fd9acf8861c5c6e5c5948ac6a1f27d
hufaga <93388988+hufaga@users.noreply.github.com>
2023-05-15 09:57:48 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.25 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -1,5 +1,5 @@
-def solution(A,B):
-    answer = 0
+# def solution(A,B):
+#     answer = 0
     # for i in range(len(A)):
     #     if max(A)>max(B):
     #         answer += max(A)*min(B)
@@ -9,13 +9,21 @@ def solution(A,B):
     #         answer += max(B)*min(A)
     #         A.remove(min(A))
     #         B.remove(max(B))  #시간 복잡도 초과
-    if max(A)>max(B):
-        A.sort(reverse=True)
-        B.sort()
-    else:
-        A.sort()
-        B.sort(reverse=True)
-    for i in range(len(A)):
-        answer += A[i]*B[i]
+    # if max(A)>max(B):
+    #     A.sort(reverse=True)
+    #     B.sort()
+    # else:
+    #     A.sort()
+    #     B.sort(reverse=True)
+    # for i in range(len(A)):
+    #     answer += A[i]*B[i]
+def solution(A, B):
+    
+    return sum([a * b for a, b in zip(sorted(A), sorted(B, reverse=True))])
+
+
+# 아래 코드는 출력을 위한 테스트 코드입니다.
+
+print(solution([1, 2], [3, 4]))
         
-    return answer
+    # return answer
