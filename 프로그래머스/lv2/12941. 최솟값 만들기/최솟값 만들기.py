e5dea706ca4ca5301975b19775a97e134130296a
hufaga <93388988+hufaga@users.noreply.github.com>
2023-05-15 09:54:10 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.44 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -0,0 +1,21 @@
+def solution(A,B):
+    answer = 0
+    # for i in range(len(A)):
+    #     if max(A)>max(B):
+    #         answer += max(A)*min(B)
+    #         A.remove(max(A))
+    #         B.remove(min(B))
+    #     else:
+    #         answer += max(B)*min(A)
+    #         A.remove(min(A))
+    #         B.remove(max(B))  #시간 복잡도 초과
+    if max(A)>max(B):
+        A.sort(reverse=True)
+        B.sort()
+    else:
+        A.sort()
+        B.sort(reverse=True)
+    for i in range(len(A)):
+        answer += A[i]*B[i]
+        
+    return answer
